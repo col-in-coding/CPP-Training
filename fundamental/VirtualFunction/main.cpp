29fe9a3ef20cd87c1c92bcf927e4161fe5e6ea88
lihaoxiang1989 <hxl.colin@gmail.com>
2020-07-07 22:03:06 +0800

    comment

--- fundamental/VirtualFunction/main.cpp
+++ fundamental/VirtualFunction/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
 class Animal{
+    /**
+     * A pure virtual function has the side effect of making its class abstract.
+     * This means that the class cannot be instantiated. 
+     * Instead, only classes that derive from the abstract class 
+     * and override the pure virtual function can be instantiated.
+     */
     virtual void Talk() const = 0;
 };
 
