09c04d7934c1094c4a44ad5b475802f77cb4766a
lihaoxiang1989 <hxl.colin@gmail.com>
2020-07-10 14:52:06 +0800

    pointer vs reference

---
+++ memoryManagement/pointer_vs_reference/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+/**
+ * Pointers can be declared without initialization. 
+ * This means we can pass an uninitialized pointer to a function 
+ * who then internally performs the initialization for us. 
+ * Pointers can be reassigned to another memory block on the heap.
+ */
+void AddFour(int &val)
+{
+    val += 4;
+}
+
+/**
+ * References are usually easier to use (depending on 
+ * the expertise level of the programmer). Sometimes however, 
+ * if a third-party function is used without properly looking 
+ * at the parameter definition, it might go unnoticed that a 
+ * value has been modified
+ */
+void AddSix(int *val)
+{
+    *val += 6; 
+}
+
+int main()
+{
+    int val = 0;
+    AddFour(val);
+    AddSix(&val);
+
+    std::cout << "val = " << val << std::endl;
+ 
+    return 0;
+}
