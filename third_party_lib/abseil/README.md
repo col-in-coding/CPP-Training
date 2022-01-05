c400d103ad3b00582d2d1b22e89cbfa27a6ad7b9
lihaoxiang1989 <hxl.colin@gmail.com>
2022-01-05 15:18:50 +0800

    test pb internel functions and debug string

---
+++ third_party_lib/abseil/README.md
@@ -0,0 +1,2 @@
+## How to run
+bazel run //main:print_my_pb
--- third_party_lib/abseil/main/print_my_pb.cc
+++ third_party_lib/abseil/main/print_my_pb.cc
@@ -1,7 +1,24 @@
+#include <iostream>
 #include "proto/addressbook.pb.h"
 
 int main(int argc, char const *argv[])
 {
     demo::AddressBook address_book;
+    demo::Person p1;
+    p1.set_name("Colin");
+    p1.set_id(1000);
+    
+    auto* my_email = p1.mutable_email();
+    *my_email = "hxl.colin@gmail.com";
+
+    auto phone1 = p1.add_phone();
+    phone1->set_number("312-307-2050");
+    phone1->set_type(demo::Person::MOBILE);
+
+    demo::Person* next_person = address_book.add_people();
+    // auto next_person = address_book.add_people();
+    next_person->CopyFrom(p1);
+    
+    std::cout << address_book.DebugString() << std::endl;
     return 0;
 }
